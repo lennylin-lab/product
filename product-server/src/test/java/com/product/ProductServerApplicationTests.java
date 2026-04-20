6db34e97bc469f87accda48c3ac07be08d839184
lenny <modhopm97b@gmail.com>
2026-04-20 18:46:17 +0800

    去除无意义的测试代码

--- product-server/src/test/java/com/product/ProductServerApplicationTests.java
+++ product-server/src/test/java/com/product/ProductServerApplicationTests.java
@@ -1,24 +1,11 @@
 package com.product;
 
-import com.product.common.core.redis.RedisCache;
-import com.product.core.utils.SecurityUtils;
-import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@Slf4j
-@SpringBootTest
 class ProductServerApplicationTests {
-    @Autowired
-    private RedisCache redisCache;
 
     @Test
     void contextLoads() {
-        String newPassword = "111111";
-        String s = SecurityUtils.encryptPassword(newPassword);
-        boolean b = SecurityUtils.matchesPassword(newPassword, s);
-        System.out.println(s);
     }
 
 }
